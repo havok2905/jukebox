655a5720affffd9725fe22600b8f452360939880
Christopher McLean <havok2905@gmail.com>
2014-10-10 02:03:09 -0400

    return json object of music librry

---
+++ ruby/library.rb
@@ -0,0 +1,37 @@
+require 'json'
+
+library_path = '/Volumes/The Void/backup/media/audio/music/'
+library = {}
+
+def music? file
+  /([^\s]+(\.(?i)(mp3|mp4|m4p|m4a))$)/.match file
+end
+
+files = Dir["#{library_path}*/*/*"]
+
+files.each do | file |
+  path_list = file.split('/').select do |dir|
+    dir != '' || dir != '.' || entry != '..'
+  end
+
+  path_list.shift( path_list.count - 3 )
+
+  artist = path_list[0]
+  album  = path_list[1]
+  song   = path_list[2]
+
+  unless library.has_key? artist
+    library[artist] = {}
+  end
+
+  unless library[artist].has_key? album
+    library[artist][album] = { songs: [] }
+  end
+
+  if music?(song) && !File.directory?(song)
+
+    library[artist][album][:songs] << { name: song.gsub(/[.mp3, .mp4, .m4p, .m4a]/, ''), path: file }
+  end
+end
+
+puts JSON.generate(library)
