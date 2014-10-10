5cfdf0ef237286812bad88e7c35e71f8acf17121
Christopher McLean <havok2905@gmail.com>
2014-10-10 17:18:29 -0400

    turned into a ruby class

--- ruby/library.rb
+++ ruby/library.rb
@@ -1,37 +1,56 @@
 require 'json'
 
-library_path = '/Volumes/The Void/backup/media/audio/music/'
-library = {}
+class Library
+  class << self
 
-def music? file
-  /([^\s]+(\.(?i)(mp3|mp4|m4p|m4a))$)/.match file
-end
+    attr_accessor :library
 
-files = Dir["#{library_path}*/*/*"]
-
-files.each do | file |
-  path_list = file.split('/').select do |dir|
-    dir != '' || dir != '.' || entry != '..'
-  end
-
-  path_list.shift( path_list.count - 3 )
-
-  artist = path_list[0]
-  album  = path_list[1]
-  song   = path_list[2]
-
-  if music?(song) && !File.directory?(file)
-
-    unless library.has_key? artist
-      library[artist] = {}
+    def music?(file)
+      /([^\s]+(\.(?i)(mp3|mp4|m4p|m4a))$)/.match file
     end
 
-    unless library[artist].has_key? album
-      library[artist][album] = { songs: [] }
+    def pretty_print(song)
+      song.gsub(/[.mp3, .mp4, .m4p, .m4a]/, '')
     end
 
-    library[artist][album][:songs] << { name: song.gsub(/[.mp3, .mp4, .m4p, .m4a]/, ''), path: file }
+    def collect_music
+      library_path = '/Volumes/The Void/backup/media/audio/music/'
+      library = Hash.new
+
+      files = Dir["#{library_path}*/*/*"]
+
+      files.each do | file |
+        path_list = file.split('/').select do |dir|
+          dir != '' || dir != '.' || entry != '..'
+        end
+
+        path_list.shift( path_list.count - 3 )
+
+        artist = path_list[0]
+        album  = path_list[1]
+        song   = path_list[2]
+
+        if music?(song) && !File.directory?(file)
+          unless library.has_key? artist
+            library[artist] = {}
+          end
+
+          unless library[artist].has_key? album
+            library[artist][album] = { songs: [] }
+          end
+
+          library[artist][album][:songs] << { name: pretty_print(song), path: file }
+        end
+      end
+
+      @library = library
+    end
+
+    def get
+      JSON.generate collect_music
+    end
   end
 end
 
-puts JSON.generate(library)
+puts Library.get
+puts Library.library
