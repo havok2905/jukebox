f4fbc9a28ca3e656fd24e5e9072aa9d7fa6aa39c
Christopher McLean <havok2905@gmail.com>
2014-10-11 19:06:32 -0400

    incorperatd grape api

---
+++ ruby/api.rb
@@ -0,0 +1,17 @@
+require 'grape'
+require './library.rb'
+
+module JukeBox
+  class API < Grape::API
+    version 'v1', using: :header, vendor: 'twitter'
+    format :json
+    prefix :api
+
+    resource :library do
+      desc 'return a a music library organizaed by artist, album, and song'
+      get :collection do
+        JukeBox::Library.collect_music
+      end
+    end
+  end
+end
--- ruby/library.rb
+++ ruby/library.rb
@@ -1,56 +1,50 @@
-require 'json'
+module JukeBox
+  class Library
+    class << self
 
-class Library
-  class << self
+      attr_accessor :library
 
-    attr_accessor :library
-
-    def music?(file)
-      /([^\s]+(\.(?i)(mp3|mp4|m4p|m4a))$)/.match file
-    end
-
-    def pretty_print(song)
-      song.gsub(/[.mp3, .mp4, .m4p, .m4a]/, '')
-    end
-
-    def collect_music
-      library_path = '/Volumes/The Void/backup/media/audio/music/'
-      library = Hash.new
-
-      files = Dir["#{library_path}*/*/*"]
-
-      files.each do | file |
-        path_list = file.split('/').select do |dir|
-          dir != '' || dir != '.' || entry != '..'
-        end
-
-        path_list.shift( path_list.count - 3 )
-
-        artist = path_list[0]
-        album  = path_list[1]
-        song   = path_list[2]
-
-        if music?(song) && !File.directory?(file)
-          unless library.has_key? artist
-            library[artist] = {}
-          end
-
-          unless library[artist].has_key? album
-            library[artist][album] = { songs: [] }
-          end
-
-          library[artist][album][:songs] << { name: pretty_print(song), path: file }
-        end
+      def music?(file)
+        /([^\s]+(\.(?i)(mp3|mp4|m4p|m4a))$)/.match file
       end
 
-      @library = library
-    end
+      def pretty_print(song)
+        song.gsub(/[.mp3, .mp4, .m4p, .m4a]/, '')
+      end
 
-    def get
-      JSON.generate collect_music
+      def collect_music
+        library_path = '/Volumes/The Void/backup/media/audio/music/'
+        library = Hash.new
+
+        files = Dir["#{library_path}*/*/*"]
+
+        files.each do | file |
+          path_list = file.split('/').select do |dir|
+            dir != '' || dir != '.' || entry != '..'
+          end
+
+          path_list.shift( path_list.count - 3 )
+
+          artist = path_list[0]
+          album  = path_list[1]
+          song   = path_list[2]
+
+          if music?(song) && !File.directory?(file)
+            unless library.has_key? artist
+              library[artist] = {}
+            end
+
+            unless library[artist].has_key? album
+              library[artist][album] = { songs: [] }
+            end
+
+            library[artist][album][:songs] << { name: pretty_print(song), path: file }
+          end
+        end
+
+        @library = library
+        library
+      end
     end
   end
 end
-
-puts Library.get
-puts Library.library
