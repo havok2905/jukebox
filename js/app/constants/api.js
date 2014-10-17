f512b9ffa89d3d243c655d91f9cc674d0535388b
Christopher McLean <havok2905@gmail.com>
2014-10-16 20:21:13 -0400

    purged angular project from ruby project

--- js/app/constants/api.js
+++
@@ -1,2 +0,0 @@
-angular.module('MediaDashboardApp')
-.constant('API_ENDPOINT', 'http://33.33.33.25:9292/api/');
--- js/app/modules/jukebox/services/library.js
+++
@@ -1,8 +0,0 @@
-angular.module('JukeBoxApp')
-.factory('Library', [ '$resource', 'API_ENDPOINT', function($resource, API_ENDPOINT) {
-  var api = $resource(API_ENDPOINT, {}, {
-    get: { method: 'get', url: API_ENDPOINT + 'library/collection', params: {} }
-  });
-
-  return api;
-}]);
