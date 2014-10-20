c228b9aaed6df2053da47d633f6a13da849bd577
Christopher McLean <havok2905@gmail.com>
2014-10-20 19:32:55 -0400

    got tests passing for the file clasS

--- ruby/file/base.rb
+++ ruby/file/base.rb
@@ -3,12 +3,13 @@ module FileItem
     class << self
       def split_path(path)
         path.split('/').select do |dir|
-          path != '' || path != '.' || path != '..'
+          dir != '' && dir != '.' && dir != '..'
         end
       end
 
       def remove_root(path, pattern)
         path.shift( path.count - pattern.count )
+        path
       end
     end
   end
---
+++ ruby/spec/file/base_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../../file/base.rb'
+
+describe FileItem do
+  context 'class methods' do
+    describe '.split_path' do
+      it 'should give me back an array of path steps' do
+        path = 'foo/bar/foobar'
+        result = FileItem::Base.split_path path
+        expect(result).to match_array(['foo', 'bar', 'foobar'])
+      end
+
+      it 'should not include directory navigators' do
+        path = '/./../foo/bar/foobar'
+        result = FileItem::Base.split_path(path)
+        expect(result.include?('')).to be_falsey
+        expect(result.include?('.')).to be_falsey
+        expect(result.include?('..')).to be_falsey
+      end
+    end
+    
+    describe '.remove_root' do
+      it 'should give me the last parts of a path' do
+        path = ['foo' ,'bar', 'foo', 'artist', 'album', 'song.mp4']
+        expected = ['artist', 'album', 'song.mp4'] 
+        pattern = ['artist', 'album', 'song' ]
+        result = FileItem::Base.remove_root path, pattern
+        expect(result).to match_array(expected)
+      end
+    end
+  end
+end
